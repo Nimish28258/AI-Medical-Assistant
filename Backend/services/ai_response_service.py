fc776ccdc63614c3c591e63a9a3f8b0a922f5769
omkar <omkar.kale@gmail.com>
2025-05-09 17:27:00 +0530

    Third commit

--- Backend/services/ai_response_service.py
+++ Backend/services/ai_response_service.py
@@ -3,3 +3,12 @@ import torch
 import torch.nn.functional as F
 from core.config import config
 
+class AIResponse:
+    def __init__(self, model_name: str = config.CLINICAL_BERT_MODEL):
+        self.device = torch.device(config.DEVICE)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=len(config.CLASS_NAMES))
+        self.model.to(self.device)
+        self.model.eval()
+
+    
